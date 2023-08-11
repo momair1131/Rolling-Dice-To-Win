7bf3952779bea456309a71db3397aea8fc627e14
momair1131 <m.omair1131@gmail.com>
2023-08-11 15:44:33 -0400

    90% completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import JSConfetti from "js-confetti";
+
 const score1El = document.querySelector("#score-0");
 const score2El = document.querySelector("#score-1");
 const dice = document.querySelector(".dice-img");
@@ -8,16 +10,22 @@ const currentP1Score = document.querySelector(".player-0-score");
 const currentP2Score = document.querySelector(".player-1-score");
 const player1 = document.querySelector(".player-0");
 const player2 = document.querySelector(".player-1");
+let canvas = document.createElement("canvas");
 
 let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playingState = true;
 
+// const jsConfetti = new JSConfetti({ canvas });
+
+// jsConfetti.addConfetti();
+
 // console.log(player1);
 score1El.textContent = 0;
 score2El.textContent = 0;
 dice.classList.add("hidden");
+canvas.classList.add("canvas");
 
 function switchPlayer() {
   document.querySelector(`.player-${activePlayer}-score`).textContent = 0;
@@ -49,9 +57,28 @@ const reset = () => {
   currentScore = 0;
 };
 
-const confettiStart = () => {
-  setTimeout(function () {});
+// CONFETTI SETTING OVERIDE
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+  // shapes: ["heart"],
+  // colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
 };
+// const count = 500,
+//   defaults = {
+//     origin: { y: 0.7 },
+//   };
+
+// function fire(particleRatio, opts) {
+//   confetti(
+//     Object.assign({}, defaults, opts, {
+//       particleCount: Math.floor(count * particleRatio),
+//     })
+//   );
+// }
 
 rollDiceBtn.addEventListener("click", () => {
   if (playingState) {
@@ -61,8 +88,7 @@ rollDiceBtn.addEventListener("click", () => {
 
     if (randomRollDice !== 1) {
       currentScore += randomRollDice;
-      document.querySelector(`.player-${activePlayer}-score`).textContent =
-        currentScore;
+      document.querySelector(`.player-${activePlayer}-score`).textContent = currentScore;
     } else {
       switchPlayer();
     }
@@ -73,18 +99,34 @@ holdBtn.addEventListener("click", () => {
   if (playingState) {
     scores[activePlayer] += currentScore;
 
-    document.querySelector(`#score-${activePlayer}`).textContent =
-      scores[activePlayer];
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
     // console.log(scores[activePlayer]);
     if (scores[activePlayer] >= 10) {
       playingState = false;
       dice.classList.add("hidden");
+      // canvas = document.querySelector(`.player-${activePlayer}`);
 
       document.querySelector(`.player-${activePlayer}`).classList.add("winner");
-      document
-        .querySelector(`.player-${activePlayer}`)
-        .classList.remove("player-active");
+      document.querySelector(`.player-${activePlayer}`).classList.remove("player-active");
       document.querySelector(`.player-${activePlayer}-score`).textContent = 0;
+      document.querySelector(`.player-${activePlayer}`).appendChild(canvas);
+      // const jsConfetti = new JSConfetti(canvas);
+      // const jsConfetti = new JSConfetti({ canvas });
+      canvas.confetti = canvas.confetti || confetti.create(canvas, { resize: true });
+      canvas.confetti({
+        spread: 80,
+        origin: { y: 1.2 },
+        particleCount: 300,
+        scalar: 1,
+        // colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+      });
+      // confetti({
+      //   // ...defaults,
+      //   particleCount: 500,
+      //   scalar: 2,
+      // });
+
+      // confetti();
     } else {
       switchPlayer();
     }
