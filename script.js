fb668755cafdcc5d1261c3fbed0c9c15df7e38ee
momair1131 <m.omair1131@gmail.com>
2023-08-14 10:11:48 -0400

    Completed Dice Game

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// import JSConfetti from "js-confetti";
-
 const score1El = document.querySelector("#score-0");
 const score2El = document.querySelector("#score-1");
 const dice = document.querySelector(".dice-img");
@@ -11,15 +9,13 @@ const currentP2Score = document.querySelector(".player-1-score");
 const player1 = document.querySelector(".player-0");
 const player2 = document.querySelector(".player-1");
 let canvas = document.createElement("canvas");
+let winScore = document.querySelector(".winning-score");
 
 let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playingState = true;
-
-// const jsConfetti = new JSConfetti({ canvas });
-
-// jsConfetti.addConfetti();
+let winningScore = 50;
 
 // console.log(player1);
 score1El.textContent = 0;
@@ -27,10 +23,11 @@ score2El.textContent = 0;
 dice.classList.add("hidden");
 canvas.classList.add("canvas");
 
+winScore.innerHTML = winningScore;
+
 function switchPlayer() {
   document.querySelector(`.player-${activePlayer}-score`).textContent = 0;
   currentScore = 0;
-  //document.getElementById("myDIV").style.transition = "all 2s";
 
   if (activePlayer === 0) {
     activePlayer = 1;
@@ -64,21 +61,7 @@ const defaults = {
   gravity: 0,
   decay: 0.94,
   startVelocity: 30,
-  // shapes: ["heart"],
-  // colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
 };
-// const count = 500,
-//   defaults = {
-//     origin: { y: 0.7 },
-//   };
-
-// function fire(particleRatio, opts) {
-//   confetti(
-//     Object.assign({}, defaults, opts, {
-//       particleCount: Math.floor(count * particleRatio),
-//     })
-//   );
-// }
 
 rollDiceBtn.addEventListener("click", () => {
   if (playingState) {
@@ -101,32 +84,22 @@ holdBtn.addEventListener("click", () => {
 
     document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
     // console.log(scores[activePlayer]);
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= winningScore) {
       playingState = false;
       dice.classList.add("hidden");
-      // canvas = document.querySelector(`.player-${activePlayer}`);
 
       document.querySelector(`.player-${activePlayer}`).classList.add("winner");
       document.querySelector(`.player-${activePlayer}`).classList.remove("player-active");
       document.querySelector(`.player-${activePlayer}-score`).textContent = 0;
       document.querySelector(`.player-${activePlayer}`).appendChild(canvas);
-      // const jsConfetti = new JSConfetti(canvas);
-      // const jsConfetti = new JSConfetti({ canvas });
+
       canvas.confetti = canvas.confetti || confetti.create(canvas, { resize: true });
       canvas.confetti({
         spread: 80,
         origin: { y: 1.2 },
         particleCount: 300,
         scalar: 1,
-        // colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
       });
-      // confetti({
-      //   // ...defaults,
-      //   particleCount: 500,
-      //   scalar: 2,
-      // });
-
-      // confetti();
     } else {
       switchPlayer();
     }
